1813da278393d251937774e486befa9d01c2385a
jemil-suleimanov <jemil1931@gmail.com>
2025-04-05 13:37:30 +0200

    refactor: restructure Electron app with Vue integration and updated configurations

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@app': '/src/app',
+            '@data': '/src/data',
+        }   
+    },
+    plugins: [vue()]
+});
